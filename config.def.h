b62a611083d219833a73aab885230d3145ca8ffd
Drew Herron <herron@mailbox.org>
2022-03-04 03:36:11 -0800

    Change default layout

--- config.def.h
+++ config.def.h
@@ -41,10 +41,10 @@ static int resizehints = 1;    /* 1 means respect size hints in tiled resizals *
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+ 	{ "[@]",      spiral },  /* first entry is default */
+	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
  	{ "[\\]",      dwindle },
 };
 
