9400a2041d52576cac409878c0ea8d8b068c7370
Drew Herron <herron@mailbox.org>
2022-07-29 01:22:18 -0700

    Bring back the bar

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 0;        /* 0 means no bar */
+static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Cascadia Mono:pixelsize=16" };
 static const char dmenufont[]       = "Cascadia Mono:pixelsize=16";
