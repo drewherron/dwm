9f18c68db2be88cdad99634f6074ee62b441e423
Drew Herron <herron@mailbox.org>
2022-08-02 14:52:37 -0700

    Add fakefullscreen patch

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
