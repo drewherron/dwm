76562e28b43fe8056756317941d17a583c19aa0b
Drew Herron <herron@mailbox.org>
2022-08-17 21:01:23 -0700

    Add window swallowing

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Cascadia Mono:pixelsize=16" };
@@ -35,10 +36,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class              instance    title       tags mask     isfloating   monitor */
-	{ "music_term",       NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "mpv",              NULL,       NULL,       0,            1,           -1 },
-	{ "guake",            NULL,       NULL,       0,            1,           -1 },
+	/* class        instance    title           tags mask  isfloating   isterminal  noswallow monitor */
+	{ "music_term", NULL,       NULL,           1 << 5,    0,           1,          0,       -1 },
+	{ "mpv",        NULL,       NULL,           0,         1,           0,          1,       -1 },
+	{ "guake",      NULL,       NULL,           0,         1,           0,          0,       -1 },
+	{ "Firefox",    NULL,       NULL,           0,         0,           0,         -1,       -1 },
+	{ "st",         NULL,       NULL,           0,         0,           1,          0,       -1 },
+	{ NULL,         NULL,       "Event Tester", 0,         0,           0,          1,       -1 }, /* xev */
 };
 
 /* layout(s) */
